16329760b1ad698b570173b893b9f59e1aad0b1b
john-henry-alfeche <alfeche.ictd@gmail.com>
2025-05-30 09:57:12 +0800

    chore(db): add mysql and postgres connection file and add env config for mysql

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,12 +2,18 @@ import { z, ZodError } from "zod";
 
 const EnvSchema = z.object({
   // Add environment variables here...
+  APP_HOST: z.string().url(),
   NEXT_PUBLIC_HOST: z.string().url(),
-  DB_HOST: z.string().url(),
+  DB_HOST: z.string(),
   DB_PORT: z.coerce.number(),
   DB_USER: z.string(),
   DB_PASS: z.string(),
   DB_NAME: z.string(),
+  HRMS_DB_HOST: z.string(),
+  HRMS_DB_PORT: z.coerce.number(),
+  HRMS_DB_USER: z.string(),
+  HRMS_DB_PASS: z.string(),
+  HRMS_DB_NAME: z.string(),
 });
 
 export type Env = z.infer<typeof EnvSchema>;
