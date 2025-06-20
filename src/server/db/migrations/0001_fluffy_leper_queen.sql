9c8c1bd23a521da701a7c8cb28aa22b0af6ff04c
eric-sison <ericsison.dev@gmail.com>
2025-06-20 13:45:05 +0800

    added more routes

---
+++ src/server/db/migrations/0001_fluffy_leper_queen.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "reading_details" ALTER COLUMN "present_reading" SET DATA TYPE double precision;--> statement-breakpoint
+ALTER TABLE "reading_details" ALTER COLUMN "usage" SET DATA TYPE double precision;--> statement-breakpoint
+ALTER TABLE "reading_details" ALTER COLUMN "billed_amount" SET DATA TYPE double precision;--> statement-breakpoint
+ALTER TABLE "reading_details" ALTER COLUMN "penalty" SET DATA TYPE double precision;
---
+++ src/server/routes/auth.ts
@@ -0,0 +1,49 @@
+import { Hono } from "hono";
+import { TokenService } from "@/lib/tokenService";
+import { zValidator } from "@hono/zod-validator";
+import { HTTPException } from "hono/http-exception";
+import { AuthSchema, loginAccounts } from "../db/schemas/loginAccounts";
+import { eq } from "drizzle-orm";
+import db from "../db/connection";
+import argon2 from "argon2";
+
+export const authHandler = new Hono()
+  .basePath("/auth")
+  .post("/register", zValidator("json", AuthSchema), async (c) => {
+    const data = c.req.valid("json");
+
+    const hashedPw = await argon2.hash(data.password);
+
+    const result = await db
+      .insert(loginAccounts)
+      .values({ ...data, password: hashedPw })
+      .returning({ userId: loginAccounts.id, username: loginAccounts.number });
+
+    return c.json(result[0]);
+  })
+
+  .post("/login", zValidator("json", AuthSchema), async (c) => {
+    const data = c.req.valid("json");
+
+    const result = await db.select().from(loginAccounts).where(eq(loginAccounts.number, data.number));
+
+    if (result.length === 0) {
+      console.error("User number not found!");
+      throw new HTTPException(401, { message: "Invalid credentials!" });
+    }
+
+    const user = result[0];
+
+    const isPasswordValid = await argon2.verify(user.password, data.password);
+
+    if (!isPasswordValid) {
+      console.error("Password is invalid!");
+      throw new HTTPException(401, { message: "Invalid credentials!" });
+    }
+
+    const tokenService = new TokenService();
+
+    const { token } = await tokenService.issueToken({ sub: user.id });
+
+    return c.json({ token });
+  });
