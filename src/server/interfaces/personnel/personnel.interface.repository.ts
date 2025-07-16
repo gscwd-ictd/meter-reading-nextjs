7eadc77a25b7a94ba6f2aeabbc286845d8d53e94
john-henry-alfeche <alfeche.ictd@gmail.com>
2025-07-16 21:29:52 +0800

    fix: update package.json and resolve merge conflict

--- src/server/interfaces/personnel/personnel.interface.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Personnel } from "@/server/types/personnel.type";
-
-export interface IPersonnelRepository {
-  findAllPersonnels(page: number, limit: number, query: string): Promise<Personnel[]>;
-  findPersonnelById(id: string): Promise<Personnel>;
-}
--- src/server/interfaces/personnel/personnel.interface.service.ts
+++
@@ -1,6 +0,0 @@
-import { Personnel } from "@/server/types/personnel.type";
-
-export interface IPersonnelService {
-  getAllPersonnels(page: number, limit: number, query: string): Promise<Personnel[]>;
-  getPersonnelById(id: string): Promise<Personnel>;
-}
