a27df7f7bda12863268b51592f8da88f2fa1ad70
John Henry Alfeche <116333206+john-henry-alfeche@users.noreply.github.com>
2025-07-17 11:11:13 +0800

    Revert "Meter reader dev"

--- src/server/interfaces/consumer/consumer.interface.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Consumer } from "@mr/server/types/consumer.type";
-
-export interface IConsumerRepository {
-  findConsumerDetails(meterReaderId: string): Promise<Consumer[]>;
-}
--- src/server/interfaces/consumer/consumer.interface.service.ts
+++
@@ -1,5 +0,0 @@
-import { Consumer } from "@mr/server/types/consumer.type";
-
-export interface IConsumerService {
-  getConsumerDetails(meterReaderId: string): Promise<Consumer[]>;
-}
