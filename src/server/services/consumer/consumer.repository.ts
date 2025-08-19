227a0840db05ec286d5e8f08cf9468ecd7110df9
eric-sison <ericsison.dev@gmail.com>
2025-08-19 09:33:09 +0800

    used today var

--- src/server/services/consumer/consumer.repository.ts
+++ src/server/services/consumer/consumer.repository.ts
@@ -3,7 +3,6 @@ import { IConsumerRepository } from "@mr/server/interfaces/consumer/consumer.int
 import { ScheduleReadingAccount, ScheduleReadingAccountSchema } from "@mr/server/types/consumer.type";
 import { and, eq } from "drizzle-orm";
 import db from "@mr/server/db/connections";
-import { format } from "date-fns";
 
 export class ConsumerRepository implements IConsumerRepository {
   async findConsumerDetails(meterReaderId: string): Promise<ScheduleReadingAccount> {
@@ -17,7 +16,8 @@ export class ConsumerRepository implements IConsumerRepository {
       .where(
         and(
           eq(consumerDetailsView.meterReaderId, meterReaderId),
-          eq(consumerDetailsView.readingDate, "08-19-2025"),
+          //MM-dd-yyyy
+          eq(consumerDetailsView.readingDate, today),
         ),
       );
 
