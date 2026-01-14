470d8825e4bddf20dc64b544f9b0a1226a505b66
john-henry-alfeche <alfeche.ictd@gmail.com>
2026-01-14 11:15:46 +0800

    update rates from decimal to real

--- src/server/db/schemas/rates.ts
+++ src/server/db/schemas/rates.ts
@@ -1,4 +1,4 @@
-import { decimal, foreignKey, integer, pgTable, real, text, unique, varchar } from "drizzle-orm/pg-core";
+import { foreignKey, integer, pgTable, real, text, unique, varchar } from "drizzle-orm/pg-core";
 import { readingDetails } from "./reading-details";
 
 export const rates = pgTable(
@@ -10,12 +10,12 @@ export const rates = pgTable(
     meterCode: integer("meter_code").notNull(),
     consumerType: varchar("consumer_type").notNull(),
     description: text("description"),
-    minimumRate: decimal("minimum_rate", { precision: 10, scale: 2 }).notNull(),
-    rate11: decimal("rate_11", { precision: 10, scale: 2 }).notNull(),
-    rate21: decimal("rate_21", { precision: 10, scale: 2 }).notNull(),
-    rate31: decimal("rate_31", { precision: 10, scale: 2 }).notNull(),
-    rate41: decimal("rate_41", { precision: 10, scale: 2 }).notNull(),
-    rate51: decimal("rate_51", { precision: 10, scale: 2 }).notNull(),
+    minimumRate: real("minimum_rate").notNull(),
+    rate11: real("rate_11").notNull(),
+    rate21: real("rate_21").notNull(),
+    rate31: real("rate_31").notNull(),
+    rate41: real("rate_41").notNull(),
+    rate51: real("rate_51").notNull(),
   },
   (t) => [
     unique().on(t.meterCode, t.consumerType),
