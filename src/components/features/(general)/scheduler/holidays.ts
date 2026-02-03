b3f5f4013034b1de96c05f7ecaf3068d4dc17a92
leekimah <mikhailsebua@gmail.com>
2026-02-03 08:53:16 +0800

    Added Chinese New Year Holiday 2026 in non business days

--- src/components/features/(general)/scheduler/holidays.ts
+++ src/components/features/(general)/scheduler/holidays.ts
@@ -159,5 +159,6 @@ export const holidays = [
 
 export const NonBusinessDays: Day[] = [
   { id: "001", date: "01-01", name: "New Year" },
-  { id: "002", date: "12-25", name: "Christmas Day" },
+  { id: "002", date: "02-17", name: "Chinese New Year 2026" }, //! REMOVE THIS. THIS IS ONLY FOR 2026
+  { id: "003", date: "12-25", name: "Christmas Day" },
 ];
